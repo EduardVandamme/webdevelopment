6765772a96d805e8b1fa6943a7c86dec830c1833
EduardVandamme <eduard.vandamme@student.vives.be>
2023-02-21 10:55:37 +0100

    commit 2

---
+++ Semester_2/JavaScriptDeel02/Opdracht_Arrays/JavaScriptFile.js
@@ -0,0 +1,9 @@
+let familieLeden = ['Eduard', 'Jerome', 'Margeaux', 'Michel', 'Sandrine'];
+console.log(familieLeden.length);
+console.log(familieLeden[0],familieLeden[2],familieLeden[4] );
+const VoegNaamToe = (arr) => {
+     familieLeden = prompt(arr);
+}
+VoegNaamToe('Bomma');
+console.log(familieLeden);
+familieLeden.toString();
